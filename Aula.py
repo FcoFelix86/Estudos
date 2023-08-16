b3dd535a98ed8cd936303f27f495e53287809412
FcoFelix86 <felix.implantacao@gmail.com>
2023-08-16 18:51:54 -0300

    Aula Python

--- Aula.py
+++ Aula.py
@@ -2,4 +2,14 @@ print('Hello World!')
 print('Hello World!')
 print('Hello World!')
 print('Atenção World!')
-print('Atenção World!')
+x = 10
+y = 2**3
+c = x/2
+print(x)
+print(y)
+print(c)
+
+num = 1
+while num <=10
+    print(num)
+    num = num + 1
