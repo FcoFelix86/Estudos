6e40c5f8a7a6e46633a5febfe199a0f4ca1af99f
FcoFelix86 <felix.implantacao@gmail.com>
2023-08-23 20:02:14 -0300

    Aula 23 08 2023

---
+++ Aula2308.py
@@ -0,0 +1,11 @@
+x = "Numero"
+y = 10
+idade = 123
+nome = "Felix"
+a = "%s tem %d anos" % (nome, idade)
+z = "Aula 2308 %d" %idade
+print(len(x)) # tamanho da palavra
+print((x[2])) # posição do caracter
+print(x+str(y))
+print(z)
+print(a)
