d7a8015b57192dcbf7df3b740df694c370acae2a
FcoFelix86 <felix.implantacao@gmail.com>
2023-08-23 21:15:16 -0300

    Aula Celsius para Fahren

--- Aula2308.py
+++ Aula2308.py
@@ -4,6 +4,7 @@ idade = 123
 nome = "Felix"
 a = "%s tem %d anos" % (nome, idade)
 z = "Aula 2308 %d" %idade
+b = {} tem {3} anos e R$ {52}
 print(len(x)) # tamanho da palavra
 print((x[2])) # posição do caracter
 print(x+str(y))
---
+++ aulaCXF.py
@@ -0,0 +1,3 @@
+temp_c = float(input("Digite a temperatudo em Celsius "))
+temp_f = temp_c * 1.8+32
+print(temp_f)
