da7d55282f82a14cac8b964580fc4f6bafc561cd
FcoFelix86 <felix.implantacao@gmail.com>
2024-09-21 16:02:30 -0300

    Geral aulas

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang= "en"
+<head>
+    <meta charset= "UTF-8">
+    <meta name= "viewpot" content= "width=device-wi"
+    <Title> Document</Title>
+</head>
+<body>
+    <h1>TESTE O PHP<h1>
+        <form action= "chama.php" method="post">
+
+        <input type= "Submit" name="ver" value="chama">
+</form>
+</body>
+</html>
